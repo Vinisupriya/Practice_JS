7fda124ccbe32d2c09cffc0e7fb2d4fc557eb055
Coolshot <vnisuprya@gmail.com>
2023-10-04 23:25:36 +0530

    Added

---
+++ Basic/fundamentals2.js
@@ -0,0 +1,17 @@
+/* Function */
+
+const calcAverage = (a+b+c) => (a+b+c)/3;
+const scoreDolphins  = calcAverage (44,23,71)
+const scoreKoalas = calcAverage (65,54,49)
+console.log (scoreDolphins ,scoreKoalas )
+
+const checkWiner = function (avgDolphins , avgKoalas){
+ if (avgDolphins < 2*avgKoalas)
+ {
+    consolre.log()
+ }else if (avgKoalas < 2*avgDolphins){
+    console.log()
+ }else{
+    console.log()
+ }
+}
