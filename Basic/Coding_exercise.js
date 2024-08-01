e809223b7aee18976ef2a1aa8212c67ac698a575
Coolshot <vnisuprya@gmail.com>
2024-08-01 21:22:10 +0530

    added js coding exercise

--- Basic/Coding_exercise.js
+++ Basic/Coding_exercise.js
@@ -1,6 +1,32 @@
-/* Ex 1 : Convert Hours into Seconds. Write a function that converts hours into seconds. 
+/* Ex 1: 
+Convert Hours into Seconds. Write a function that converts hours into seconds. 
 
 function howManySeconds(hours) {
 	let x = (hours * 60)* 60
 	return x
-}; */
+}; 
+Ex 2:
+Return the Sum of Two Numbers. 
+Create a function that takes two numbers as arguments and returns their sum.
+
+function addition(a, b) {
+	let c = a + b;
+	return c
+};
+Ex 3:   
+Create a function that takes voltage and current and returns the calculated power.
+
+function circuitPower(voltage, current) {
+	let power = voltage * current;
+	return power
+};
+Ex 4: ARRAY - Return the First Element in an Array
+Create a function that takes an array containing only numbers and return the first element.
+function getFirstValue(arr) {
+	return arr[0];
+};
+Ex 5:Convert Age to Days.
+Create a function that takes the age in years and returns the age in days.
+function calcAge(age) {
+	return age * 365;
+} */
