0d908c47a9d9a48e330d7995142e386e711a4988
Coolshot <vnisuprya@gmail.com>
2024-08-03 21:16:04 +0530

    update Coding_exercise.js

--- Basic/Coding_exercise.js
+++ Basic/Coding_exercise.js
@@ -29,4 +29,5 @@ Ex 5:Convert Age to Days.
 Create a function that takes the age in years and returns the age in days.
 function calcAge(age) {
 	return age * 365;
+Ds - Array, Objects, Maps and sets.
 } */
